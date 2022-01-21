5b772c21d68a7238ac60c7670072381cdb39466f
Lotuses-robot <87472564+Lotuses-robot@users.noreply.github.com>
2022-01-21 18:50:52 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ headers={'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) Ap
 
 s = requests.session()
 s.headers = headers
-lastt=0
+lastt=lasttj=lasttq=0
 
 def try_getj():
     while True:
@@ -29,7 +29,7 @@ def try_getj():
     return j
 
 while True:
-    mxt=-1
+    mxt=mxtj=mxtq=-1
     t=int(time.time()*1000-2000)
     # print(t)
     j=s.get('https://map.oiercraft.ga:20684/up/world/world/'+str(t),verify=False).content
@@ -38,6 +38,8 @@ while True:
     except:
         time.sleep(3)
         continue
+
+    # find chat
 
     l=j.find('"type": "chat"')
     
@@ -55,7 +57,53 @@ while True:
             mxt=max(mxt,lst['timestamp'])
 
         l=j.find('"type": "chat"',l+1)
+    
     if mxt!=-1:
         lastt=mxt
+
+    #find join
+
+    l=j.find('"type": "playerjoin"')
+    
+    while l!=-1:
+        l2=j.find('"timestamp":',l)
+        l2+=len('"timestamp": 1642663591149}')
+        try:
+            lst=json.loads(j[l-1:l2])
+        except:
+            pass
+
+        if lst['timestamp']>lasttj:
+            s.get('http://127.0.0.1:5700/send_group_msg?group_id=865811340&message='+lst['account']+' 加入了服务器。')
+            print(lst['timestamp'],lasttj,lst['account']+' 加入了服务器。'+'\n')
+            mxtj=max(mxtj,lst['timestamp'])
+
+        l=j.find('"type": "playerjoin"',l+1)
+    
+    if mxtj!=-1:
+        lasttj=mxtj
+        
+    #find quit
+
+    l=j.find('"type": "playerquit"')
+    
+    while l!=-1:
+        l2=j.find('"timestamp":',l)
+        l2+=len('"timestamp": 1642663591149}')
+        try:
+            lst=json.loads(j[l-1:l2])
+        except:
+            pass
+
+        if lst['timestamp']>lasttq:
+            s.get('http://127.0.0.1:5700/send_group_msg?group_id=865811340&message='+lst['account']+' 退出了服务器')
+            print(lst['timestamp'],lasttq,lst['account']+' 退出了服务器'+'\n')
+            mxtq=max(mxtq,lst['timestamp'])
+
+        l=j.find('"type": "playerquit"',l+1)
+    
+    if mxtq!=-1:
+        lasttq=mxtq
+    
     #ttime.sleep(3)
     
